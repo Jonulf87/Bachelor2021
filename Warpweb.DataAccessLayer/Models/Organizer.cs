0b9fe92c3a63b4deca421f572dbdd2b5a52d07f1
Arne S <arnesorli@gmail.com>
2021-01-26 12:05:25 +0100

    Added Organizer class

---
+++ Warpweb.DataAccessLayer/Models/Organizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warpweb.DataAccessLayer.Models
+{
+    class Organizer
+    {
+        public int OrganizerId { get; set; }
+        public string OrganizerName { get; set; }
+        public string OrganizerCode { get; set; }
+        public string OrganizerDescription { get; set; } // VAT/MVA code etc.
+        public ApplicationUser OrganizerContactPerson { get; set; }
+
+    }
+}
