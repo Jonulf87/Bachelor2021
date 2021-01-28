0edbe5afe922e563a6aa453a9b80dfedd640e310
Arne S <arnesorli@gmail.com>
2021-01-28 20:51:37 +0100

    Added test project for unit testing

---
+++ WarpTest/OrganizerTest.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WarpTest
+{
+    public class OrganizerTest
+    {
+        [Fact]
+        public void CreateOrganizer()
+        {
+
+        }
+    }
+}
