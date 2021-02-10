cb07333c67ce68e64dcc731c75e1c04e0450b099
Victoria Weber <wevi8619@gmail.com>
2021-02-10 18:56:08 +0100

    mainpage

---
+++ Warpweb.WebLayer/ClientApp/src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+﻿import React, { Fragment } from 'react';
+
+const layout = (props) => (
+    <Fragment>
+        <div>Toolbar</div>
+        <main>
+            {props.children}
+        </main>
+    </Fragment>
+);
+
+export default layout;
---
+++ Warpweb.WebLayer/ClientApp/src/components/MainPage/MainPage.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import './MainPage.css'  
+
+const main = (props) => (
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/"> Home</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+);
+
+export default main;
