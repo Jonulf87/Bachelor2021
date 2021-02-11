d7cab4f87d483b8e864390892c347555bdfba883
Arne S <arnesorli@gmail.com>
2021-02-11 13:39:47 +0100

    Modified CrewService and Exceptions

--- Warpweb.LogicLayer/Exceptions/CrewDoesNotExistException.cs
+++ Warpweb.LogicLayer/Exceptions/CrewDoesNotExistException.cs
@@ -8,6 +8,16 @@ namespace Warpweb.LogicLayer.Exceptions
 {
     public class CrewDoesNotExistException : Exception
     {
+        public CrewDoesNotExistException()
+        {
+        }
 
+        public CrewDoesNotExistException(string message) : base(message)
+        {
+        }
+
+        public CrewDoesNotExistException(string message, Exception inner) : base(message, inner)
+        {
+        }
     }
 }
--- Warpweb.LogicLayer/Services/CrewService.cs
+++ Warpweb.LogicLayer/Services/CrewService.cs
@@ -48,14 +48,11 @@ namespace Warpweb.LogicLayer.Services
         // TODO: Try - catch w/logging
         public async Task<int> CreateCrewAsync(CrewVm crewVm)
         {
-            var existingCrew = _dbContext.Crews
+            try
+            {
+                var existingCrew = _dbContext.Crews
                 .Where(a => a.CrewId == crewVm.CrewId || a.CrewName == crewVm.CrewName)
                 .FirstOrDefault();
-
-                if (existingCrew != null)
-                {
-                    throw new CrewAlreadyExistsException();
-                }
 
                 var crew = new Crew
                 {
@@ -68,6 +65,12 @@ namespace Warpweb.LogicLayer.Services
                 await _dbContext.SaveChangesAsync();
 
                 return crew.CrewId;
+            }
+            catch (CrewDoesNotExistException e)
+            {
+                _log.LogInformation(e.Message);
+                return 0;
+            }        
         }
 
         public async Task<int> UpdateCrewAsync(CrewVm crewVm)
@@ -76,7 +79,7 @@ namespace Warpweb.LogicLayer.Services
 
             if(existingCrew == null)
             {
-                throw new NotImplementedException();
+                throw new CrewDoesNotExistException("Det finnes ingen crew med denne IDen.");
             }
 
             existingCrew.CrewId = crewVm.CrewId;
@@ -97,7 +100,7 @@ namespace Warpweb.LogicLayer.Services
 
             if (crewToBeDeleted == null)
             {
-                throw new NotImplementedException();
+                throw new CrewDoesNotExistException();
             }
 
             _dbContext.Remove<Crew>(crewToBeDeleted);
