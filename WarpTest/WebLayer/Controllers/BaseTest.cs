050025539f89935dc93287caa073869e89df086b
Victoria Weber <wevi8619@gmail.com>
2021-02-13 15:00:19 +0100

    BaseTest

---
+++ WarpTest/WebLayer/Controllers/BaseTest.cs
@@ -0,0 +1,51 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using System.Data.Common;
+using Warpweb.DataAccessLayer;
+using Warpweb.DataAccessLayer.Models;
+
+namespace WarpTest.WebLayer.Controllers
+{
+    public class BaseTest
+    {
+        protected DbConnection _connection;
+        protected DbContextOptions _options;
+        protected ApplicationDbContext _dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            CreateInMemoryDatabase();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _connection.Dispose();
+        }
+
+        private void CreateInMemoryDatabase()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+
+            _connection.Open();
+
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _dbContext = new ApplicationDbContext(_options, Options.Create<OperationalStoreOptions>(new OperationalStoreOptions()));
+            _dbContext.Database.EnsureDeleted();
+            _dbContext.Database.EnsureCreated();
+
+            _dbContext.CrewRoles.Add(new CrewRole { CrewRoleId = 1, Description = "Test Rolle 1" });
+            _dbContext.CrewRoles.Add(new CrewRole { CrewRoleId = 2, Description = "Test Rolle 2" });
+            _dbContext.CrewRoles.Add(new CrewRole { CrewRoleId = 3, Description = "Test Rolle 3" });
+
+            _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ WarpTest/WebLayer/Controllers/CrewRoleControllerTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using Warpweb.LogicLayer.Services;
+using Warpweb.LogicLayer.ViewModels;
+using Warpweb.WebLayer.Controllers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace WarpTest.WebLayer.Controllers
+{
+    class CrewRoleControllerTest : BaseTest
+    {
+        [Test]
+        public async Task ShouldGetCrewRoles()
+        {
+            CrewRoleService crewRoleService = new CrewRoleService(_dbContext);
+            CrewRoleController crewRoleController = new CrewRoleController(crewRoleService);
+
+            // 3 roles have been added in CreateInMemoryDatabase() in BaseTest
+
+            List<CrewRoleListVm> result = await crewRoleController.GetCrewRoles();
+
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(1, result[0].CrewRoleId);
+            Assert.AreEqual(2, result[1].CrewRoleId);
+            Assert.AreEqual(3, result[2].CrewRoleId);
+        }
+
+        [Test]
+        public async Task ShouldGetCrewRoleById()
+        {
+            CrewRoleService crewRoleService = new CrewRoleService(_dbContext);
+            CrewRoleController crewRoleController = new CrewRoleController(crewRoleService);
+
+            ActionResult<CrewRoleVm> result1 = await crewRoleController.GetCrewRole(1);
+            CrewRoleVm returnedCrewRole1 = result1.Value;
+            Assert.AreEqual(1, returnedCrewRole1.CrewRoleId);
+            Assert.AreEqual("Test Rolle 1", returnedCrewRole1.Description);
+
+            ActionResult<CrewRoleVm> result2 = await crewRoleController.GetCrewRole(2);
+            CrewRoleVm returnedCrewRole2 = result2.Value;
+            Assert.AreEqual(2, returnedCrewRole2.CrewRoleId);
+            Assert.AreEqual("Test Rolle 2", returnedCrewRole2.Description);
+        }
+
+
+        // Helper methods
+        // await crewRoleController.CreateCrewRole(new CrewRoleVm { Description = "Test role 1" });
+        // await crewRoleController.CreateCrewRole(new CrewRoleVm { Description = "Test role 2" });
+        // await crewRoleController.CreateCrewRole(new CrewRoleVm { Description = "Test role 3" });
+    }
+}
