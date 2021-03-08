81e79a142c83bc3db83309f9a01ed32ce3c73123
Victoria Weber <victoria.weber.no@gmail.com>
2021-03-08 21:08:43 +0100

    Create Venue frontend

--- Warpweb.LogicLayer/ViewModels/VenueVm.cs
+++ Warpweb.LogicLayer/ViewModels/VenueVm.cs
@@ -5,6 +5,7 @@
         public int VenueId { get; set; }
         public string VenueName { get; set; }
         public string VenueAddress { get; set; }
+        public string PostalCode { get; set; }
         public int VenueAreaAvailable { get; set; }
         public int VenueCapacity { get; set; }
         public int ContactId { get; set; }
--- Warpweb.WebLayer/ClientApp/src/components/Venue/CreateVenue.js
+++ Warpweb.WebLayer/ClientApp/src/components/Venue/CreateVenue.js
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+﻿import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
@@ -6,19 +6,80 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import authService from '../api-authorization/AuthorizeService';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         '& .MuiTextField-root': {
             margin: theme.spacing(1),
             width: '25ch',
-        },
-    },
+        }
+    }
 }));
 
 export default function CreateVenue() {
 
     const classes = useStyles();
+
+    const isMounted = useRef(true);
+
+    const [isSending, setIsSending] = useState(false);
+    const [name, setName] = useState('');
+    const [contactId, setContactId] = useState('');
+    const [address, setAddress] = useState('');
+    const [postalCode, setPostalCode] = useState('');
+    const [area, setArea] = useState('');
+    const [capacity, setCapacity] = useState('');
+
+    useEffect(
+        () => {
+            return () => {
+                isMounted.current = false
+            }
+        },
+        []
+    );
+
+    const sendRequest = async () => {
+        // don't send again while we are sending
+        if (isSending) {
+            return;
+        }
+
+        // update state
+        setIsSending(true);
+
+        const data = {
+            'VenueName': name,
+            'VenueAddress': address,
+            'PostalCode': postalCode,
+            'VenueAreaAvailable': area,
+            'VenueCapacity': capacity,
+            'ContactId': contactId
+        }
+
+        const authenticationResult = await authService.isAuthenticated();
+        if (authenticationResult) {
+            const accessToken = await authService.getAccessToken();
+            const response = await fetch('/api/Venue', {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(data)
+            });
+            const result = await response.json();
+            console.log(result);
+        } else {
+            alert("Not authorized");
+        }
+
+        // only update if we are still mounted
+        if (isMounted.current) {
+            setIsSending(false);
+        }
+    };
 
     return (
         <Card className={classes.root}>
@@ -30,7 +91,9 @@ export default function CreateVenue() {
                     <div>
                         <div>
                             <TextField
-                                id="outlined-full-width"
+                                onChange={e => setName(e.target.value)}
+                                id="name"
+                                name="name"
                                 label="Navn"
                                 style={{ margin: 8 }}
                                 placeholder="Navn på lokale"
@@ -43,7 +106,9 @@ export default function CreateVenue() {
                             />
 
                             <TextField
-                                id="outlined-full-width"
+                                onChange={e => setContactId(e.target.value)}
+                                id="contactperson"
+                                name="contactperson"
                                 label="Kontaktperson"
                                 style={{ margin: 8 }}
                                 placeholder="Kontakterson"
@@ -57,35 +122,41 @@ export default function CreateVenue() {
 
                         </div>
                         <div>
-                        <TextField
-                            id="outlined-full-width"
-                            label="Adresse"
-                            style={{ margin: 8 }}
-                            placeholder="Gateadresse"
-                            fullWidth
-                            margin="normal"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            variant="outlined"
-                        />
+                            <TextField
+                                onChange={e => setAddress(e.target.value)}
+                                id="address"
+                                name="address"
+                                label="Adresse"
+                                style={{ margin: 8 }}
+                                placeholder="Gateadresse"
+                                fullWidth
+                                margin="normal"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                variant="outlined"
+                            />
 
-                        <TextField
-                            id="outlined-full-width"
-                            label="Postnr"
-                            style={{ margin: 8 }}
-                            placeholder="Postnummer"
-                            fullWidth
-                            margin="normal"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            variant="outlined"
-                        />
+                            <TextField
+                                onChange={e => setPostalCode(e.target.value)}
+                                id="postalcode"
+                                name="postalcode"
+                                label="Postnr"
+                                style={{ margin: 8 }}
+                                placeholder="Postnummer"
+                                fullWidth
+                                margin="normal"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                variant="outlined"
+                            />
                         </div>
                         <div>
                             <TextField
-                                id="outlined-full-width"
+                                onChange={e => setArea(e.target.value)}
+                                id="area"
+                                name="area"
                                 type="Number"
                                 label="Areal"
                                 style={{ margin: 8 }}
@@ -99,7 +170,9 @@ export default function CreateVenue() {
                             />
 
                             <TextField
-                                id="outlined-full-width"
+                                onChange={e => setCapacity(e.target.value)}
+                                id="capacity"
+                                name="capacity"
                                 type="Number"
                                 label="Kapasitet"
                                 style={{ margin: 8 }}
@@ -120,6 +193,8 @@ export default function CreateVenue() {
                     size="large"
                     className={classes.button}
                     startIcon={<SaveIcon />}
+                    disabled={isSending}
+                    onClick={sendRequest}
                 >
                     Lagre
                 </Button>
