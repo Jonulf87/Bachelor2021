44eeb1dbe9ce2b92f576ab145a71d622cd47c16d
Jone Lode <jone.lode@outlook.com>
2021-03-11 13:01:31 +0100

    Added ignored file. It was an accident. I promise.

---
+++ Warpweb.LogicLayer/ViewModels/UserRolesListVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warpweb.LogicLayer.ViewModels
+{
+    public class UserRolesListVm
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool UserHasRole { get; set; } //Dette er en kommentar
+        public int Integer { get; set; }
+    }
+}
