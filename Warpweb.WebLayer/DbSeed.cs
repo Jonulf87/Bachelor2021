ec5b25f46cd3eac8dceee3bf29f5da81688ae866
Jone Lode <jone.lode@outlook.com>
2021-03-19 11:33:32 +0100

    updated db seed

--- Warpweb.WebLayer/DbSeed.cs
+++ Warpweb.WebLayer/DbSeed.cs
@@ -33,72 +33,7 @@ namespace Warpweb.WebLayer
             var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
             var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
 
-            // Opprett noen arrangører
-            var organizer = new Organizer
-            {
-                Name = "Warpcrew",
-                OrgNumber = "123456789",
-                Description = "Warpcrew er en ting."
-            };
-
-            var organizer2 = new Organizer
-            {
-                Name = "CarpWred",
-                OrgNumber = "34567891011",
-                Description = "Bedre enn WarpCrew"
-            };
-
-            dbContext.Organizers.Add(organizer);
-            dbContext.Organizers.Add(organizer2);
-            dbContext.SaveChanges(); //Savechanges skriver automatisk Id tilbake til eksisterende objekt.
-
-            //Lager noen brukere
-            // Må legge inn AdminRoleAtOrganizer for å få CreateEvent til å virke?
-
-            //var organizerList = new List<Organizer>();
-
-            //organizerList.Add(organizer);
-
-            var user = new ApplicationUser
-            {
-                FirstName = "Post",
-                LastName = "Man",
-                Email = "postmanwarpweb@gmail.com",
-                EmailConfirmed = true,
-                PhoneNumber = "+111111111111",
-                PhoneNumberConfirmed = true,
-                LockoutEnabled = false,
-                UserName = "postmanwarpweb@gmail.com"
-                
-            };
-
-            var user2 = new ApplicationUser
-            {
-                FirstName = "Jan",
-                MiddleName = "Petter",
-                LastName = "Hansen",
-                Email = "Hansen69@mail.com",
-                EmailConfirmed = true,
-                PhoneNumber = "45454545",
-                PhoneNumberConfirmed = true,
-                LockoutEnabled = false,
-                UserName = "Hansen69@mail.com",
-                Address = "Hjemme"
-
-            };
-
-            var user3 = new ApplicationUser
-            {
-                FirstName = "Ole",
-                LastName = "Brum",
-                Email = "olebole247@gmail.com",
-                EmailConfirmed = true,
-                PhoneNumber = "+111111111111",
-                PhoneNumberConfirmed = true,
-                LockoutEnabled = false,
-                UserName = "olebole247@gmail.com"
-            };
-
+            //Opprette noen roller
             // Sjekk om rolle eksisterer
             var roleSuperAdminExist = await roleManager.RoleExistsAsync("SuperAdmin");
             var roleAdminExist = await roleManager.RoleExistsAsync("Admin");
@@ -108,7 +43,7 @@ namespace Warpweb.WebLayer
             if (!roleSuperAdminExist)
             {
                 var role = new IdentityRole();
-                role.Name = "SuperAdmin";
+                role.Name = "Superadmin";
                 await roleManager.CreateAsync(role);
             }
 
@@ -126,101 +61,219 @@ namespace Warpweb.WebLayer
                 await roleManager.CreateAsync(role);
             }
 
-            // Sett e-post/passord og rolle
+            //Opprett noen brukere
 
-            var userExist = await userManager.FindByEmailAsync(user.Email);
-
-            if (userExist == null)
+            var userPostman = new ApplicationUser
             {
-                var result = await userManager.CreateAsync(user, "SuperHemmelig");
-                if (!result.Succeeded)
-                {
-                    Console.WriteLine("Failed to create user #1");
-                }
-
-                var roleResult = await userManager.AddToRoleAsync(user, "SuperAdmin");
-
-                if (!roleResult.Succeeded)
-                {
-                    Console.WriteLine("Failed to add user #1 to role");
-                }
-
-            };     
-
-            var user2Exist = await userManager.FindByEmailAsync(user2.Email);
-
-            if (user2Exist == null)
-            {
-                var result = await userManager.CreateAsync(user2, "SuperHemmelig");
-                if (!result.Succeeded)
-                {
-                    Console.WriteLine("Failed to create user #2");
-                }
-
-                var roleResult = await userManager.AddToRoleAsync(user2, "Admin");
-                if (!roleResult.Succeeded)
-                {
-                    Console.WriteLine("Failed to add user #1 to role");
-                }
-
+                FirstName = "Post",
+                MiddleName = "Hey Mr.",
+                LastName = "Man",
+                Email = "postmanwarpweb@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+111111111111",
+                PhoneNumberConfirmed = true,
+                LockoutEnabled = false,
+                UserName = "postmanwarpweb@gmail.com",
+                Address = "Oslogaten 13",
+                IsAllergic = true,
+                AllergyDescription = "Tåler ikke nøtter og skalldyr.",
+                Comments = "Må reise seg hvert femte minutt, hvis ikke stivner kneet i beinprotesen."
             };
 
-            var user3Exist = await userManager.FindByEmailAsync(user3.Email);
-
-            if (user3Exist == null)
+            var userJanHansen = new ApplicationUser
             {
-                var result = await userManager.CreateAsync(user3, "IkkeHemmelig");
-                if (!result.Succeeded)
-                {
-                    Console.WriteLine("Failed to create user #3");
-                }
-
-                var roleResult = await userManager.AddToRoleAsync(user3, "User");
-                if (!roleResult.Succeeded)
-                {
-                    Console.WriteLine("Failed to add user #1 to role");
-                }
+                FirstName = "Jan",
+                MiddleName = "Petter",
+                LastName = "Hansen",
+                Email = "Hansen69@mail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "45454545",
+                PhoneNumberConfirmed = true,
+                LockoutEnabled = false,
+                UserName = "Hansen69@mail.com",
+                Address = "Hjemme",
+                IsAllergic = false,
+                Comments = "Jeg liker skilpadder."
             };
 
-            // Ekstra roller for user
-            await userManager.AddToRoleAsync(user, "Admin");
-            await userManager.AddToRoleAsync(user, "User");
+            var userOleBrumm = new ApplicationUser
+            {
+                FirstName = "Ole",
+                LastName = "Brum",
+                Email = "olebole247@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+111111111111",
+                PhoneNumberConfirmed = true,
+                LockoutEnabled = false,
+                UserName = "olebole247@gmail.com",
+                IsAllergic = false,
+                Comments = "You never saw me here."
+            };
 
-            // Ekstra rolle for user2
-            await userManager.AddToRoleAsync(user2, "SuperAdmin");
+            var userKristian = new ApplicationUser
+            {
+                FirstName = "Kristian",
+                MiddleName = "oooohhh... look at me",
+                LastName = "Pettersen",
+                Email = "pettersen@mail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+45983245",
+                PhoneNumberConfirmed = true,
+                LockoutEnabled = false,
+                UserName = "pettersen@mail.com",
+                Address = "Fjaseveien 51",
+                IsAllergic = true,
+                AllergyDescription = "Tåler ikke Ole Brumm.",
+                Comments = "Hater Ole Brumm"
+            };
+
+            var userChristian = new ApplicationUser
+            {
+                FirstName = "Kristian",
+                MiddleName = "Christian",
+                LastName = "Kristiansen",
+                Email = "Kristiansen@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+6578451298",
+                PhoneNumberConfirmed = true,
+                LockoutEnabled = false,
+                UserName = "kristiansen@gmail.com",
+                Address = "I kjelleren til mamma og pappa",
+                IsAllergic = true,
+                AllergyDescription = "Allergisk mot idioti.",
+                Comments = "Får kraftig utslett i nærheten av idioter."
+            };
+
+            List<ApplicationUser> users = new List<ApplicationUser>();
+
+            users.Add(userPostman);
+            users.Add(userKristian);
+            users.Add(userChristian);
+            users.Add(userOleBrumm);
+            users.Add(userJanHansen);
+
+            foreach (ApplicationUser user in users)
+            {
+                if(await userManager.FindByEmailAsync(user.Email) == null)
+                {
+                    if(user.Email == "postmanwarpweb@gmail.com")
+                    {
+                        await userManager.CreateAsync(user, "SuperHemmelig");
+                        await userManager.AddToRoleAsync(user, "Superadmin");
+                        await userManager.AddToRoleAsync(user, "Admin");
+                        await userManager.AddToRoleAsync(user, "User");
+                    }
+
+                    await userManager.CreateAsync(user, "SuperHemmelig");
+                    await userManager.AddToRoleAsync(user, "Admin");
+                    await userManager.AddToRoleAsync(user, "User");
+                }
+            }
+
+            //Liste over admins til Waprcrew
+            List<ApplicationUser> adminsListWarpcrew = new List<ApplicationUser>();
+            adminsListWarpcrew.Add(userPostman);
+            adminsListWarpcrew.Add(userJanHansen);
+
+            //Liste over admins til Kandu
+            List<ApplicationUser> adminsListKandu = new List<ApplicationUser>();
+            adminsListKandu.Add(userChristian);
+            adminsListKandu.Add(userKristian);
+
+            //Liste over admins til Hordalan
+            List<ApplicationUser> adminsListHordalan = new List<ApplicationUser>();
+            adminsListHordalan.Add(userOleBrumm);
+
+            // Opprett noen arrangører
+            var organizerWarpcrew = new Organizer
+            {
+                Name = "Warpcrew",
+                OrgNumber = "123456789",
+                Description = "Warpcrew er en ting.",
+                Admins = adminsListWarpcrew
+            };
+
+            var organizerKandu = new Organizer
+            {
+                Name = "Kandu",
+                OrgNumber = "34567891011",
+                Description = "Bedre enn WarpCrew.",
+                Admins = adminsListKandu
+            };
+
+            var organizerHordalan = new Organizer
+            {
+                Name = "Hordalan",
+                OrgNumber = "324098712304987",
+                Description = "Uti skjærgården et sted.",
+                Admins = adminsListHordalan
+            };
+
+            dbContext.Organizers.Add(organizerWarpcrew);
+            dbContext.Organizers.Add(organizerKandu);
+            dbContext.Organizers.Add(organizerHordalan);
+            dbContext.SaveChanges();
+
 
             // Legg til noen lokaler
-            var venue = new Venue
+            var venueRockefeller = new Venue
             {
                 Name = "Rockefeller",
                 Address = "Torggata Bad 1",
+                AreaAvailable = 56,
+                Capacity = 3,
+                Users = adminsListWarpcrew
             };
 
-            var venue2 = new Venue
+            var venueSpektrum = new Venue
             {
                 Name = "Spektrum",
-                Address = "Ikke Torggata Bad 1"
+                Address = "Ikke Torggata Bad 1",
+                AreaAvailable = 1000,
+                Capacity = 1000,
+                Users = adminsListKandu
             };
 
-            dbContext.Venues.Add(venue);
-            dbContext.Venues.Add(venue2);
+            var venueVikingskipet = new Venue
+            {
+                Name = "Vikingskipet",
+                Address = "Hamargaten 1",
+                AreaAvailable = 1500,
+                Capacity = 4000,
+                Users = adminsListHordalan
+            };
+
+            dbContext.Venues.Add(venueRockefeller);
+            dbContext.Venues.Add(venueSpektrum);
+            dbContext.Venues.Add(venueVikingskipet);
             dbContext.SaveChanges();
 
             // Legg til noen events
-            var mainEvent = new MainEvent
+            var mainEventWarpzone = new MainEvent
             {
                 Name = "WarpZone 21",
-                OrganizerId = organizer.Id,
+                OrganizerId = organizerWarpcrew.Id,
+                Venue = venueRockefeller
             };
 
-            var mainEvent2 = new MainEvent
+            var mainEventTheGathering = new MainEvent
             {
-                Name = "BygdeLan 21",
-                OrganizerId = organizer2.Id,
+                Name = "TG 21",
+                OrganizerId = organizerKandu.Id,
+                Venue = venueSpektrum
             };
 
-            dbContext.MainEvents.Add(mainEvent);
-            dbContext.MainEvents.Add(mainEvent2);
+            var mainEventHordalan = new MainEvent
+            {
+                Name = "Hordalan 21",
+                OrganizerId = organizerHordalan.Id,
+                Venue = venueVikingskipet
+            };
+
+
+            dbContext.MainEvents.Add(mainEventWarpzone);
+            dbContext.MainEvents.Add(mainEventTheGathering);
+            dbContext.MainEvents.Add(mainEventHordalan);
             dbContext.SaveChanges();
   
         }
