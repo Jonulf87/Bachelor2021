2edc0404bbd1e6252075bfadf670706753e6b716
Arne S <arnesorli@gmail.com>
2021-03-29 20:32:18 +0200

    Added Swagger for API testing and documentation. Swagger UI at localhost:<port>/swagger

--- Warpweb.WebLayer/Startup.cs
+++ Warpweb.WebLayer/Startup.cs
@@ -18,6 +18,8 @@ using Warpweb.DataAccessLayer.Interfaces;
 using Warpweb.DataAccessLayer.Models;
 using Warpweb.LogicLayer.Services;
 using Warpweb.WebLayer.Configs;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace Warpweb.WebLayer
 {
@@ -52,6 +54,43 @@ namespace Warpweb.WebLayer
             services.AddHttpContextAccessor();
             services.AddTransient<IMainEventProvider, MainEventProvider>();
 
+            // Enable Swagger for API documentation
+            services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation  
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "JWT Token Authentication API",
+                    Description = "Warpweb - .NET 5 Web API"
+                });
+
+                // To Enable authorization using Swagger (JWT)  
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
+            });
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -138,6 +177,16 @@ namespace Warpweb.WebLayer
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Warpweb API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
