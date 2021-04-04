7faf560ea05602d457cc20d216cf16b313737813
Victoria Weber <victoria.weber.no@gmail.com>
2021-04-04 12:43:29 +0200

    Fix it

--- Warpweb.WebLayer/ClientApp/src/components/User/UserPicker.js
+++ Warpweb.WebLayer/ClientApp/src/components/User/UserPicker.js
@@ -27,6 +27,7 @@ export default function UserPicker() {
 
     return (
         <Dialog>
+            /*
             <FixedSizeList>
                 {usersList.map((user) => (
                     <ListItem key={user.id}>
@@ -40,6 +41,7 @@ export default function UserPicker() {
                 ))}
 
             </FixedSizeList>
+            */
         </Dialog>
     )
 }
