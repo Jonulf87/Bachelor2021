42211ea3cba8c683616713c9f37cf1dc197e2f10
AslakBS <aslakbs@yahoo.no>
2021-04-22 17:10:35 +0200

    URL access check on Admin-components. Moved Switch from App to Approuter.js

--- Warpweb.WebLayer/ClientApp/src/components/ErrorPages/Unauthorized.js
+++ Warpweb.WebLayer/ClientApp/src/components/ErrorPages/Unauthorized.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Grid, Paper, Typography } from '@material-ui/core';
+import {Grid, Typography } from '@material-ui/core';
 
 
-export default function PageNotFound() {
+export default function Unauthorized() {
     return (
         <Grid container justify="center" alignItems="center">
             <Grid item>
                 <Typography align="center" variant="h4" component="h2">
-                    Denne siden finnes ikke
+                    Du har ikke ikke tilgang til denne siden
                 </Typography>
                 <Typography align="center" variant="h6" component={Link} to="/">
                     Gå til framsiden
