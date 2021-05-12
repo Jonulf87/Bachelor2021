6cb61413f0fc0ad5f27bb1e27386752dd3039209
Arne S <arnesorli@gmail.com>
2021-05-12 10:57:13 +0200

    Added errordialog to fetch block in EventUserMain

--- Warpweb.WebLayer/ClientApp/src/components/Event/EventUserMain.js
+++ Warpweb.WebLayer/ClientApp/src/components/Event/EventUserMain.js
@@ -2,7 +2,7 @@
 import { List, ListItem } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import EventCard from './EventCard';
-import { CallMissedSharp } from '@material-ui/icons';
+import PopupWindow from '../PopupWindow/PopupWindow';
 
 const useStyles = makeStyles({
     root: {
@@ -11,8 +11,19 @@ const useStyles = makeStyles({
 });
 
 export default function EventUserMain() {
+
+    //Statevariabler for error popup vindu
+    const [error, setError] = useState();
+    const [errors, setErrors] = useState([]);
+    const [errorDialogOpen, setErrorDialogOpen] = useState(false);
+
     const [eventsList, setEventsList] = useState([]);
     const [isReady, setIsReady] = useState(false);
+
+    //Metode for error popup vindu
+    const handleErrorDialogClose = () => {
+        setErrorDialogOpen(false);
+    }
 
     const classes = useStyles();
 
@@ -20,9 +31,24 @@ export default function EventUserMain() {
         const getEvents = async () => {
 
             const responseEvents = await fetch('/api/events/upcomingevents');
-            const resultEvents = await responseEvents.json();
-            setEventsList(resultEvents);
-            setIsReady(true);
+
+            if (responseEvents.ok) {
+                const resultEvents = await responseEvents.json();
+                setEventsList(resultEvents);
+                setIsReady(true);
+
+            }
+            else if (responseEvents.status === 400) {
+                const errorResult = await responseEvents.json();
+                setErrors(errorResult.errors);
+                setErrorDialogOpen(true);
+            }
+            else {
+                const errorResult = await responseEvents.json();
+                setError(errorResult.message);
+                setErrorDialogOpen(true);
+            }
+
         }
         getEvents();
     }, []);
@@ -30,6 +56,8 @@ export default function EventUserMain() {
     return (
         <List
         >
+            <PopupWindow open={errorDialogOpen} handleClose={handleErrorDialogClose} error={error} clearError={setError} errors={errors} clearErrors={setErrors} />
+
             {isReady && eventsList.sort((a, b) => (a.startDateTime > b.startDateTime) ? 1 : (a.startDateTime < b.startDateTime) ? -1 : 0).map((event) => (
                 <ListItem className={classes.root} key={event.id}>
                     <EventCard {...event} />
